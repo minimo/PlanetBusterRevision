48e0109c34171192fec86550fae147f3fe5b2275
minimo <minimo.mobile@gmail.com>
2015-10-07 18:32:43 +0900

    update asset

--- src/assets.js
+++ src/assets.js
@@ -8,17 +8,27 @@
 var assets = [];
 
 //初期読み込みアセット
-assets["IPL"] = {
+assets["common"] = {
     image: {
-    },
-    spritesheet: {
+        "tex1":     "assets/images/tex1.png",
+        "tex2":     "assets/images/tex2.png",
+        "tex_boss1":"assets/images/tex_boss1.png",
+        "gunship":  "assets/images/gunship1.png",
+        "effect":   "assets/images/effect.png",
     },
     sound: {
-        "stage1":   "assets/sounds/expsy.mp3",
         "warning":  "assets/sounds/bgm_warning.mp3",
     },
     font: {
         "UbuntuMono":       "fonts/UbuntuMono-Bold.ttf",
         "Orbitron-Regular": "fonts/Orbitron-Regular.ttf",
     }
+};
+
+assets["stage1"] = {
+    image: {
+    },
+    sound: {
+        "stage1":   "assets/sounds/expsy.mp3",
+    },
 };
