a5ca0d007188f71f073afdfa1f1e9c1aeb9794f3
minimo <minimo.mobile@gmail.com>
2015-10-08 17:34:43 +0900

    update loading scene

--- src/scene/loadingscene.js
+++ src/scene/loadingscene.js
@@ -7,18 +7,73 @@
 
 //アセットロード用シーン
 phina.define("pbr.LoadingScene", {
-    superClass: 'phina.game.LoadingScene',
+    superClass: 'phina.display.CanvasScene',
 
-    init: function(param) {
-        this.superInit({
-            assets: pbr.Application.assets[param.assetType],
+    init: function(options) {
+        var _default = {
+            asset: pbr.Application.assets[options.assetType],
             width: SC_W,
             height: SC_H,
             lie: false,
             exitType: "auto",
-        });
-    },
+        };
+        options = (options||{}).$safe(_default);
+        this.superInit(options);
+        var labelParam = {
+            text: "Loading",
+            fill: "white",
+            stroke: true,
+            strokeColor: 'black',
+            strokeWidth: 3,
 
-    update: function(app) {
+            fontFamily: "Orbitron",
+            align: "center",
+            baseline: "middle",
+            fontSize: 20
+        };
+        phina.display.Label(labelParam)
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.5);
+
+        this.fromJSON({
+            children: {
+                gauge: {
+                    className: 'phina.ui.Gauge',
+                    arguments: {
+                        value: 0,
+                        width: this.width,
+                        height: 12,
+                        color: '#aaa',
+                        stroke: false,
+                        gaugeColor: 'hsla(200, 100%, 80%, 0.8)',
+                        padding: 0,
+                    },
+                    x: this.gridX.center(),
+                    y: 0,
+                    originY: 0,
+                }
+            }
+        });
+
+        var loader = phina.asset.AssetLoader();
+        if (options.lie) {
+            this.gauge.animationTime = 10*1000;
+            this.gauge.value = 90;
+            loader.onload = function() {
+                this.gauge.animationTime = 1*1000;
+                this.gauge.value = 100;
+            }.bind(this);
+        } else {
+            loader.onprogress = function(e) {
+                this.gauge.value = e.progress*100;
+            }.bind(this);
+        }
+        this.gauge.onfull = function() {
+            if (options.exitType === 'auto') {
+                this.app.popScene();
+            }
+        }.bind(this);
+
+        loader.load(options.asset);
     },
 });
