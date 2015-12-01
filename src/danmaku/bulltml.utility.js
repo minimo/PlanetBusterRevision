81d159d63e69afcf887ae26150d69b4f9e2334ae
minimo <minimo.mobile@gmail.com>
2015-12-01 10:27:43 +0900

    add bulletML  utility

---
+++ src/danmaku/bulltml.utility.js
@@ -0,0 +1,34 @@
+/*
+ *  bulletml.utility.js
+ *  2015/12/01
+ *  @auther minimo  
+ *  This Program is MIT license.
+ */
+
+phina.namespace(function() {
+    bulletml.dsl.global = function() {
+        $action = bulletml.dsl.action;
+        $actionRef = bulletml.dsl.actionRef;
+        $bullet = bulletml.dsl.bullet;
+        $bulletRef = bulletml.dsl.bulletRef;
+        $fire = bulletml.dsl.fire;
+        $fireRef = bulletml.dsl.fireRef;
+        $changeDirection = bulletml.dsl.changeDirection;
+        $changeSpeed = bulletml.dsl.changeSpeed;
+        $accel = bulletml.dsl.accel;
+        $wait = bulletml.dsl.wait;
+        $vanish = bulletml.dsl.vanish;
+        $repeat = bulletml.dsl.repeat;
+        $bindVar = bulletml.dsl.bindVar;
+        $notify = bulletml.dsl.notify;
+        $direction = bulletml.dsl.direction;
+        $speed = bulletml.dsl.speed;
+        $horizontal = bulletml.dsl.horizontal;
+        $vertical = bulletml.dsl.vertical;
+        $fireOption = bulletml.dsl.fireOption;
+        $offsetX = bulletml.dsl.offsetX;
+        $offsetY = bulletml.dsl.offsetY;
+        $autonomy = bulletml.dsl.autonomy;
+    }
+});
+
