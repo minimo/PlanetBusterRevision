c663be144e959107ef86cdcacde3581149c78d1e
minimo <minimo.mobile@gmail.com>
2016-02-22 17:40:24 +0900

    update stage data

--- src/stage/stage1.js
+++ src/stage/stage1.js
@@ -24,8 +24,6 @@ phina.define("pbr.Stage1", {
             this.player.isAfterburner = false;
         });
 
-        this.add( 120, "ToyBox-p-right");
-
         //Stage data
         this.add( 180, "Hornet1-left");
         this.add(  60, "Hornet1-right");
@@ -42,13 +40,13 @@ phina.define("pbr.Stage1", {
         this.add(  20, "Hornet1-right");
         this.add( 120, "Hornet1-center");
 
-        this.add(120, function(app) {
-            this.ground.tweener.clear().to({scaleX:0.5, scaleY:0.5, speed:2.0}, 300, "easeInOutCubic");
-        });
-
         this.add(  90, "Hornet2-left");
         this.add( 120, "Hornet2-right");
         this.add( 120, "Hornet2-center");
+
+        this.add(120, function(app) {
+            this.ground.tweener.clear().to({scaleX:0.5, scaleY:0.5, speed:2.0}, 300, "easeInOutCubic");
+        });
 
         this.add(  30, "BigWing-left");
         this.add( 180, "BigWing-right");
