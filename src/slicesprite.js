a3dd88ef8c0f3e4983c256a6af657e5e353309e2
minimo <minimo.mobile@gmail.com>
2016-03-18 19:01:42 +0900

    update slice sprite

--- src/slicesprite.js
+++ src/slicesprite.js
@@ -10,7 +10,7 @@ phina.define("phina.display.SliceSprite", {
 
     init: function(image, width, height, option) {
         this.superInit();
-        option = option.$safe({
+        this.option = option.$safe({
             sliceX: 2,  //横分割数
             sliceY: 2,  //縦分割数
             trimming: { //トリミング情報
@@ -20,6 +20,10 @@ phina.define("phina.display.SliceSprite", {
                 height: height,
             },
         });
+
+        this.image = image;
+        this.width = width;
+        this.height = height;
 
         this.sprites = [];
         var i = 0;
@@ -36,7 +40,30 @@ phina.define("phina.display.SliceSprite", {
     },
 
     //バラバラ
-    fallApart: function() {
+    fallApart: function(func) {
+        func = func || function(e) {
+            var sx = (e.x==0?0:(e.x>0?1:-1));
+            var sy = (e.y==0?0:(e.y>0?1:-1));
+            e.tweener.clear()
+                .to({x: e.x*10, y:e.y*10, alpha: 0}, 1000, "easeOutSine");
+        }
+        this.sprites.each(func);
+        return this;
+    },
+
+    //元に戻す
+    reinstate: function() {
+        var i = 0;
+        var cw = ~(this.width/this.option.sliceX);
+        var ch = ~(this.height/this.option.sliceY);
+        for (var y = 0; y < this.option.sliceY; y++) {
+            for (var x = 0; x < this.option.sliceX; x++) {
+                s.rotation = 0;
+                s.setPosition(x*cw-this.width/2, y*ch-this.height/2);
+                i++;
+            }
+        }
+        return this;
     },
 });
 
