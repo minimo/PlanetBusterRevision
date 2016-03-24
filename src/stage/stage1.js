96d059ea11fa389f86a3b91e9c4cd054b0dde4e9
minimo <minimo.mobile@gmail.com>
2016-03-24 18:12:44 +0900

    update stage data

--- src/stage/stage1.js
+++ src/stage/stage1.js
@@ -24,15 +24,14 @@ phina.define("pbr.Stage1", {
             this.player.isAfterburner = false;
         });
 
-        this.add( 120, "ToyBox-p-right");
         this.add( 300, "ThorHammer", {boss: true});
         this.add( 120, function(app) {
             this.ground.tweener.clear().to({speed:10.0}, 180, "easeInOutCubic");
             this.player.isAfterburner = true;
         });
         this.add( 1800, function() {});
-        this.add( 300, function(app) {
-            this.ground.tweener.clear().to({speed:3.0}, 180, "easeInOutCubic");
+        this.add( 30, function(app) {
+            this.ground.tweener.clear().to({speed:5.0}, 180, "easeInOutCubic");
             this.player.isAfterburner = false;
         });
 
@@ -56,17 +55,33 @@ phina.define("pbr.Stage1", {
         this.add( 120, "Hornet2-right");
         this.add( 120, "Hornet2-center");
 
+        //中ボス
+        this.add( 300, "ThorHammer", {boss: true});
+        this.add( 120, function(app) {
+            this.ground.tweener.clear().to({speed:10.0}, 180, "easeInOutCubic");
+            this.player.isAfterburner = true;
+        });
+        this.add( 1800, function() {});
+        this.add( 30, function(app) {
+            this.ground.tweener.clear().to({speed:5.0}, 180, "easeInOutCubic");
+            this.player.isAfterburner = false;
+        });
+
+        this.add( 180, "Hornet1-left");
+        this.add(  60, "Hornet1-right");
+        this.add(  60, "Hornet1-center");
+
+        this.add( 120, "Hornet1-left");
+        this.add(   1, "Hornet1-right");
+        this.add(  60, "Hornet1-center");
+
         this.add(120, function(app) {
             this.ground.tweener.clear().to({scaleX:0.5, scaleY:0.5, speed:2.0}, 600, "easeInOutSine");
         });
 
         this.add(  30, "BigWing-left");
         this.add( 180, "BigWing-right");
-/*
-        this.add(120, function(app) {
-            this.ground.tweener.clear().to({scaleX:1.0, scaleY:1.0, speed:3.0}, 300, "easeInOutCubic");
-        });
-*/
+
         this.add( 120, "Hornet2-left");
         this.add(  20, "Hornet2-right");
         this.add( 120, "Hornet2-center");
