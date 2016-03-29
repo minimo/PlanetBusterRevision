3e183e23929d574569dcd3d79408fb4b3c105d50
minimo <minimo.mobile@gmail.com>
2016-03-29 12:10:11 +0900

    add gameover scene

---
+++ src/scene/gameoverscene.js
@@ -0,0 +1,86 @@
+/*
+ *  gameoverscene.js
+ *  2016/03/29
+ *  @auther minimo  
+ *  This Program is MIT license.
+ */
+ 
+tm.define("pbr.GameoverScene", {
+    superClass: "phina.display.DisplayScene",
+
+    _member: {
+        labelParam: {
+            fill: "white",
+            stroke: "black",
+            strokeWidth: 1,
+
+            fontFamily: "UbuntuMono",
+            align: "left",
+            baseline: "middle",
+            fontSize: 20,
+            fontWeight: ''
+        },
+    },
+
+    init: function(score, stageNumber, boss, allclear) {
+        this.superInit();
+        this.$extend(this._member);
+
+        //バックグラウンド
+        var param = {
+            width:SC_W*0.7,
+            height:SC_H*0.5,
+            fill: "rgba(0,0,0,0.7)",
+            stroke: "rgba(0,0,0,0.7)",
+            backgroundColor: 'transparent',
+        };
+        this.bg = phina.display.RectangleShape(param)
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.5)
+
+        //リザルト表示
+        this.result1 = "SCORE: "+score;
+        if (!allclear) {
+            this.result2 = "Stage:"+stageNumber+(boss?"-boss":"");
+        } else {
+            this.result2 = "(ALL CLEAR!!)";
+        }
+
+        //ゲームオーバー表示
+        phina.display.Label({text: "GAME OVER"}.$safe(this.labelParam))
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.4);
+
+        phina.display.Label({text: this.result1}.$safe(this.labelParam))
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.5);
+
+        phina.display.Label({text: this.result2}.$safe(this.labelParam))
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.55);
+
+        phina.display.Label({text: "Press [Z] key or Touch"}.$safe(this.labelParam))
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.60);
+
+        this.time = 0;        
+    },
+
+    update: function() {
+        //キーボード操作
+        var kb = app.keyboard;
+        if (this.time > 60 && app.keyboard.getKey("Z")) {
+            this.exit();
+        }
+        this.time++;
+    },
+
+    ontouchstart: function(e) {
+        this.exit();
+    },
+
+    exit: function() {
+        app.replaceScene(pbr.TitleScene());
+    },
+});
+
