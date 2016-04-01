18d78829b01d57687bf65d9f24bbb8c36433ad98
minimo <minimo.mobile@gmail.com>
2016-04-01 17:48:19 +0900

    add gameover scene

---
+++ src/scene/gameoverscene.js
@@ -0,0 +1,89 @@
+/*
+ *  GameOverScene.js
+ *  2014/06/04
+ *  @auther minimo  
+ *  This Program is MIT license.
+ *
+ */
+
+phina.define("pbr.GameObverScene", {
+    superClass: "phina.display.DisplayScene",
+    
+    _member: {
+        //ラベル用パラメータ
+        titleParam: {
+            text: "",
+            fill: "white",
+            stroke: "blue",
+            strokeWidth: 2,
+
+            fontFamily: "Orbitron",
+            align: "center",
+            baseline: "middle",
+            fontSize: 32,
+            fontWeight: ''
+        },
+        msgParam: {
+            text: "",
+            fill: "white",
+            stroke: false,
+            strokeWidth: 2,
+
+            fontFamily: "Orbitron",
+            align: "center",
+            baseline: "middle",
+            fontSize: 15,
+            fontWeight: ''
+        },
+    },
+
+    init: function() {
+        this.superInit();
+        this.$extend(this._member);
+
+        //バックグラウンド
+        var param = {
+            width:SC_W,
+            height:SC_H,
+            fill: 'black',
+            stroke: false,
+            backgroundColor: 'transparent',
+        };
+        this.bg = phina.display.RectangleShape(param)
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.5)
+        this.bg.tweener.setUpdateType('fps');
+
+        phina.display.Label({text: "GAME OVER"}.$safe(this.titleParam))
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.4);
+
+        phina.display.Label({text: "Press[Z]key or touch to Title"}.$safe(this.msgParam))
+            .addChildTo(this)
+            .setPosition(SC_W*0.5, SC_H*0.6);
+
+        this.time = 0;
+    },
+    
+    update: function(app) {
+        //キーボード操作
+        var kb = this.app.keyboard;
+        if (this.time > 30 && app.keyboard.getKey("Z")) {
+            this.exit();
+        }
+        this.time++;
+    },
+
+    //タッチorクリック開始処理
+    onpointstart: function(e) {
+    },
+
+    //タッチorクリック移動処理
+    onpointmove: function(e) {
+    },
+
+    //タッチorクリック終了処理
+    onpointend: function(e) {
+        this.exit();
+    },
+});
