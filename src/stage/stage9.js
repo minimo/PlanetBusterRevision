539badbbf52046aa4b5e81d0ee6c93ce16735287
minimo <minimo.mobile@gmail.com>
2016-10-21 17:39:06 +0900

    Add stage 2 boss enter pattern.

--- src/stage/stage9.js
+++ src/stage/stage9.js
@@ -20,7 +20,12 @@ phina.define("pbr.Stage9", {
             app.playBGM("stage9", true);
         });
 
-        this.add( 180, "Raven");
+//        this.add( 180, "Raven");
+        this.add( 180, function() {
+//            this.event["boss_shadow"].value.call();
+           this.event["boss_enter"].value.call();
+        }.bind(this));
+
 
         //ガルーダ機影
         this.addEvent("boss_shadow", function() {
@@ -31,12 +36,60 @@ phina.define("pbr.Stage9", {
                 .setFrameTrimming(112, 320, 296, 160)
                 .setFrameIndex(0)
                 .setPosition(SC_W*1.1, SC_H*1.2)
-                .setScale(3.0);
+                .setScale(2.0);
             shadow.update = function() {
                 this.y -= 1;
                 if (this.y < -80) this.remove();
             };
         });
+
+        //ボス登場演出
+        this.addEvent("boss_enter", function() {
+            var shadow = phina.display.Sprite("tex_boss1Black", 296, 80)
+                .setFrameTrimming(112, 320, 296, 160)
+                .setFrameIndex(0)
+                .setPosition(SC_W*0.5, SC_H*0.4);
+            shadow.layer = LAYER_SHADOW;
+            shadow.alpha = 0.0;
+            shadow.time = 0;
+            shadow.addChildTo(app.currentScene);
+
+            shadow.tweener.setUpdateType('fps').clear()
+                .to({alpha: 0.4, y: SC_H*0.2}, 300, "easeOutSine")
+                .wait(120)
+                .call(function() {
+                })
+                .wait(120)
+                .call(function() {
+                    this.remove();
+                });
+
+            shadow.update = function() {
+                this.time++;
+                if (this.time < 300) return; 
+
+                //煙もくもくー
+                var x1 = this.x;
+                var y1 = this.y;
+                var x2 = this.x+140;
+                var y2 = this.y+40;
+                var vy = this.parentScene.ground.deltaY;
+                for (var r = 0; r < 2; r++) {
+                    if (r == 1) x2 = this.x-140;
+                    for (var i = 0; i < 3; i++) {
+                        var p = Math.randfloat(0, 1.0);
+                        var px = Math.floor(x1*p+x2*(1-p))+16;
+                        var py = Math.floor(y1*p+y2*(1-p))-32;
+                        var layer = this.parentScene.effectLayerLower;
+                        layer.enterSmokeLarge({
+                            position: {x: px, y: py},
+                            velocity: {x: 0, y: vy+Math.randint(0, 3), decay: 1.01},
+                            delay: rand(0, 2)
+                        });
+                    }
+                }
+            }
+        }.bind(this));
 
         //WARNING
         this.addEvent("warning", function() {
